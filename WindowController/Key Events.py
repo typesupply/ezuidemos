9c273e8b8e919213df3b9df8698ea0c25e2f908d
Tal Leming <tal@typesupply.com>
2026-02-14 15:32:46 -0500

    Update to new API.

--- WindowController/Key Events.py
+++ WindowController/Key Events.py
@@ -14,9 +14,9 @@ class DemoWindow(ezui.WindowController):
         self.panel = None
         content = """
         Try these key combinations:
-        t
-        t shift
-        t command shift
+        T
+        T shift
+        T command shift
         """
         descriptionData = dict()
         self.w = ezui.EZWindow(
@@ -24,54 +24,34 @@ class DemoWindow(ezui.WindowController):
             descriptionData=descriptionData,
             controller=self
         )
-        self.w.addKeyDownBinding(
-            character="t",
-            callback=self.keyDownTCallback
+        self.w.addKeyBinding(
+            character="T",
+            callback=self.keyTCallback,
+            title="Documentation for T."
         )
-        self.w.addKeyDownBinding(
-            character="t",
+        self.w.addKeyBinding(
+            character="T",
             modifiers=["shift"],
-            callback=self.keyDownTShiftCallback
+            callback=self.keyTShiftCallback,
+            title="Documentation for T + shift."
         )
-        self.w.addKeyDownBinding(
-            character="t",
+        self.w.addKeyBinding(
+            character="T",
             modifiers=["shift", "command"],
-            callback=self.keyDownTShiftCommandCallback
-        )
-        self.w.addKeyUpBinding(
-            character="t",
-            callback=self.keyUpTCallback
-        )
-        self.w.addKeyUpBinding(
-            character="t",
-            modifiers=["shift"],
-            callback=self.keyUpTShiftCallback
-        )
-        self.w.addKeyUpBinding(
-            character="t",
-            modifiers=["shift", "command"],
-            callback=self.keyUpTShiftCommandCallback
+            callback=self.keyTShiftCommandCallback,
+            title="Documentation for T + shift + command."
         )
 
     def started(self):
         self.w.open()
 
-    def keyDownTCallback(self, sender):
+    def keyTCallback(self, sender):
         print("down: t")
 
-    def keyDownTShiftCallback(self, sender):
+    def keyTShiftCallback(self, sender):
         print("down: t shift")
 
-    def keyDownTShiftCommandCallback(self, sender):
+    def keyTShiftCommandCallback(self, sender):
         print("down: t shift command")
-
-    def keyUpTCallback(self, sender):
-        print("up: t")
-
-    def keyUpTShiftCallback(self, sender):
-        print("up: t shift")
-
-    def keyUpTShiftCommandCallback(self, sender):
-        print("up: t shift command")
 
 DemoWindow()
